e6ce960be5b04b7c2fbdfe6f8743e9bf4e8b0c43
Th. Androutsos <a8anassis@gmail.com>
2020-07-01 00:19:29 +0300

    Hello Thanos

--- src/gr/aueb/ch1/HelloThan.java
+++ src/gr/aueb/ch1/HelloThan.java
@@ -2,6 +2,6 @@ package gr.aueb.ch1;
 
 public class HelloThan {
     public static void main(String[] args) {
-        System.out.println("Hello Than");
+        System.out.println("Hello Thanos");
     }
 }
